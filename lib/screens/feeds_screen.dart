929cf2654aa25773a9ad8d231c1791f167c4fb82
Alin Babu <alinbabu2010@gmail.com>
2023-03-16 16:21:54 +0530

    Add article list view into feed screen file and remove separate widget.
    Add scrollToTop function to scroll on content refresh

--- lib/screens/feeds_screen.dart
+++ lib/screens/feeds_screen.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter_news/utils/constants.dart';
-import 'package:flutter_news/widgets/article_list.dart';
 import 'package:flutter_news/widgets/error_widget.dart';
 import 'package:provider/provider.dart';
 
 import '../provider/news_provider.dart';
 import '../provider/news_state.dart';
+import '../utils/dimensions.dart';
+import '../widgets/article_item.dart';
 import '../widgets/progress_bar.dart';
 
 class FeedScreen extends StatefulWidget {
@@ -17,10 +18,33 @@ class FeedScreen extends StatefulWidget {
 }
 
 class _FeedScreenState extends State<FeedScreen> {
+  final ScrollController _scrollController = ScrollController();
+
   void _showSnackBar(String message) {
     ScaffoldMessenger.of(context)
       ..removeCurrentSnackBar()
       ..showSnackBar(SnackBar(content: Text(message)));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(_scrollListener);
+  }
+
+  void _scrollListener() {
+    if (_scrollController.position.maxScrollExtent ==
+        _scrollController.position.pixels) {
+      final newsProvider = Provider.of<NewsProvider>(context, listen: false);
+      if (!newsProvider.newsState.isLoadMore) {
+        newsProvider.fetchData();
+      }
+    }
+  }
+
+  void _scrollToTop() {
+    _scrollController.animateTo(0,
+        duration: const Duration(milliseconds: 500), curve: Curves.linear);
   }
 
   Future<void> _onRefresh(bool isLoadingMore) async {
@@ -40,6 +64,7 @@ class _FeedScreenState extends State<FeedScreen> {
       body: Consumer<NewsProvider>(
         builder: (context, provider, child) {
           final newsState = provider.newsState;
+          final articles = newsState.articles ?? [];
           if (newsState.status == Status.loading) {
             return const ProgressBar();
           } else if (newsState.isError &&
@@ -52,15 +77,31 @@ class _FeedScreenState extends State<FeedScreen> {
           return RefreshIndicator(
             onRefresh: () async {
               await _onRefresh(newsState.isLoadMore);
+              _scrollToTop();
             },
             child: newsState.isError && newsState.articles?.isEmpty == true
                 ? EmptyMsgWidget(
                     message: newsState.message!,
                     isScrollNeeded: true,
                   )
-                : ArticleList(
-                    articles: newsState.articles ?? [],
-                    state: newsState,
+                : ListView.builder(
+                    controller: _scrollController,
+                    padding: const EdgeInsets.only(top: Dimens.listTopPadding),
+                    itemBuilder: (context, index) {
+                      final article =
+                          index < articles.length ? articles[index] : null;
+                      return article != null
+                          ? ArticleItem(article: article)
+                          : newsState.isNoMoreData
+                              ? const EmptyMsgWidget(
+                                  message: Constants.noMoreData,
+                                  isScrollNeeded: false,
+                                )
+                              : const ProgressBar();
+                    },
+                    itemCount: newsState.isLoadMore || newsState.isNoMoreData
+                        ? articles.length + 1
+                        : articles.length,
                   ),
           );
         },
